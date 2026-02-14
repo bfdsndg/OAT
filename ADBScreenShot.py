28ef77de112f15197108298e4ded85ebd1738057
bfdsndg <260133646@qq.com>
2026-02-14 23:11:37 +0800

    去掉没用的代码

--- ADBScreenShot.py
+++ ADBScreenShot.py
@@ -10,39 +10,7 @@ from main import CONFIG, logger
 class Core:
     Adb_path = '0'
     Adb_port = 0
-    @staticmethod                                                                                            
-    def validate_adb_path(adb_path=CONFIG["adb"]["path"]) -> Optional[str]:
-        """
-        验证用户输入的ADB路径是否有效
-        :param adb_path: 用户输入的ADB路径（可带引号/空格）
-        :return: 有效路径返回绝对路径，无效返回None
-        期望是只在最初配置时候执行一次
-        事实上我根本没用这个东西
-        """
-        adb_path = adb_path.strip().strip('"').strip("'")
     
-        if not os.path.exists(adb_path):
-            logger.error(f"错误：ADB路径不存在！输入的路径：{adb_path}")
-            return None
-    
-    # 检查是否是adb.exe
-        if not os.path.isfile(adb_path) or not adb_path.endswith(("adb.exe", "HD-Adb.exe")):
-            logger.error(f"错误：路径不是有效的ADB可执行文件！需指向 adb.exe 或 HD-Adb.exe")
-            return None
-    
-    # 验证ADB可执行
-        try:
-            subprocess.run(
-                [adb_path, "--version"],  # 执行adb --version验证
-                shell=True, capture_output=True, timeout=5
-            )
-        except Exception as e:
-            logger.error(f"错误：ADB文件不可执行！原因：{str(e)}")
-            return None
-    
-        # print(f"ADB路径验证通过：{adb_path}")
-        return adb_path
-
     @staticmethod
     def adb_screenshot( save_path=CONFIG["resource"]["screenshot_path"],
         adb_path=CONFIG["adb"]["path"],
@@ -88,4 +56,5 @@ class Core:
     
         except Exception as e:
             logger.error(f"截图过程异常：{str(e)}")
+
             return False
